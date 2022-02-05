e06655bab04a6383f80f6e90341c7cf7d982c27c
Ritsch Ulrich <ritschulrich@live.de>
2022-02-05 13:51:24 +0100

    Code Refactor

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import demjson
 import ssl
 import re
 import math
+import csv
 from urllib import request
 from lxml import html
+from functools import partial
 
 def getHtmlFromRequest(url):
   ctx = ssl.create_default_context()
@@ -13,26 +15,28 @@ def getHtmlFromRequest(url):
   response = request.urlopen(url, context=ctx)
   return html.fromstring(response.read())
 
-if __name__ == "__main__":
-  allianceRequestUrl = "https://lordsandknights.enjoyed.today/tools/AllianceMembers/default.aspx?alliance=l+k://alliance?1155&260"
-  playersUrl = "https://lordsandknights.enjoyed.today/PlayerCastles/default.aspx?player="
-
-  allicanceHtmlData = getHtmlFromRequest(allianceRequestUrl)
-
-  scriptTags = allicanceHtmlData.xpath("//script[@type='text/javascript']")
+def getPlayersContent(allianceHtmlData):
+  scriptTags = allianceHtmlData.xpath("//script[@type='text/javascript']")
 
   playersContent = scriptTags[7].text_content()
 
   playersContent = playersContent.strip()
-  playersContent = playersContent.replace("\n", "")
+  return playersContent.replace("\n", "")
 
+def getPlayersAsVector (playersContent):
   playersMatch = re.search("var players=(\[.*?\])", playersContent)
 
-  players = demjson.decode(playersMatch.group(1))
+  return demjson.decode(playersMatch.group(1))
 
+def getPlayers (allianceLink):
+  allicanceHtmlData = getHtmlFromRequest(allianceRequestUrl + allianceLink)
+  playersContent = getPlayersContent(allicanceHtmlData)
+  return getPlayersAsVector(playersContent)
+
+def getPlayersHabitate(players):
   habitate = []
 
-  for player in players:
+  for player in enemyPlayers:
     playersHtmlData = getHtmlFromRequest(playersUrl + player["link"])
 
     playerHabitate = playersHtmlData.xpath("//table[@id='PlayerCastles']/tr")
@@ -48,16 +52,34 @@ if __name__ == "__main__":
                        "coordinate-y": int(coordinateMatch.group(2)),
                        "points": points})
 
-  # {'coordinateLink': 'l+k://coordinates?16303,16442&260', 'coordinate-x': 16303, 'coordinate-y': 16442, 'points': '290'}
-  # (x2-x1)^2+(y2-y1)^2 (Wurzel ziehen)
+  return habitate
 
-  castles = list(filter(lambda habitat : (len(habitat["points"]) <= 3 and
-                                          math.sqrt(pow(16303 - habitat["coordinate-x"], 2) + pow(16442 - habitat["coordinate-y"], 2)) <= 5), habitate))
+def filterOnlyCastles(habitat):
+  return len(habitat["points"]) <= 3
 
-  with open("coordinates.txt", "w+", encoding="utf8") as file:
-    file.write(str(castles))
+def filterCastlesInHotspot(habitat, hotspots):
+  possibleHotspot = list(filter(lambda hotspot: math.sqrt(pow(hotspot["x"] - habitat["coordinate-x"], 2) + pow(hotspot["y"] - habitat["coordinate-y"], 2)) <= hotspot["r"], hotspots))
+  return len(possibleHotspot) > 0
+
+if __name__ == "__main__":
+  enemyAllianceLink = "l+k://alliance?1155&260"
+  allianceRequestUrl = "https://lordsandknights.enjoyed.today/tools/AllianceMembers/default.aspx?alliance="
+  playersUrl = "https://lordsandknights.enjoyed.today/PlayerCastles/default.aspx?player="
+
+  enemyPlayers = getPlayers(enemyAllianceLink)
+  enemyHabitate = getPlayersHabitate(enemyPlayers)
+
+  hotspots = [{"x": 16269, "y": 16419, "r": 10}, {"x": 16303, "y": 16442, "r": 5}]
+
+  enemyCastles = list(filter(filterOnlyCastles, enemyHabitate))
+  enemyCastlesOnlyInHotspot = list(filter(lambda habitat : filterCastlesInHotspot(habitat, hotspots), enemyHabitate))
+
+  with open("enemyCoordinates.txt", "w+", encoding="utf8") as file:
+    file.write(str(enemyCastlesOnlyInHotspot))
 
   ## todo's
+  ### python main.py -enemyHotspots [{x: 16444, y: 16533, r: 5}, {x: 16410, y: 16522, r: 10}] -playersInput players.csv -faken 3
+  ### mehrere Coordinaten und andere Radien eingeben und auswerten
   ### CSV erstellen, indem alle Spieler aufgelistet werden, die bei Krieg mitmachen
   ### Jeder Spieler bekommt schrittweise die Links verteilts. (änhlich zu Uno)
   ### Als Parameter einen Bereich definieren, wie viele Castels angegriffen werden dürfen.
